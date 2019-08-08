3490147d361462f22f56442f800d4a91d285b55e
Gabriel <gabriel.luchtenberg12@gmail.com>
2019-08-07 21:03:19 -0300

    Add second round robin test

--- src/services/round-robin/__tests__/assign-games.test.ts
+++ src/services/round-robin/__tests__/assign-games.test.ts
@@ -11,4 +11,16 @@ describe("Round robin", () => {
     const result = roundRobin(tournamentPeople);
     expect(result).toMatchObject(expectedResult);
   });
+  it("Should return full tournaent 2", () => {
+    const tournamentPeople = ["Antonio", "Valim", "Gabriel", "Ana", "Maria"];
+    const result = roundRobin(tournamentPeople);
+    const expectedResult: string[][][] = [
+      [["Valim", "Maria"], ["Gabriel", "Ana"]],
+      [["Antonio", "Maria"], ["Valim", "Gabriel"]],
+      [["Antonio", "Ana"], ["Maria", "Gabriel"]],
+      [["Antonio", "Gabriel"], ["Ana", "Valim"]],
+      [["Antonio", "Valim"], ["Ana", "Maria"]]
+    ];
+    expect(result).toMatchObject(expectedResult);
+  });
 });
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type RoundRobin = [[string[]]];
+
+interface Challenger {
+  name: String;
+}
+// export type KeyTournament = [Challenger[]];
+// export
+// // export interface RoundRobin {
+// //   challengers: ;
+// // }
+// // export interface KeyTournament {
+// //     challengers: string[]
+// // }
+// export interface TournamentKeys {
+//   challengers: any[];
+// }
